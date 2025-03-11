da46955cffb014b443e5ab7c163c3394a4972e2e
Erik S. Carlsten <ecarlste@gmail.com>
2025-03-11 10:19:29 -0600

    extract task board into component

---
+++ src/app/_components/task-board.tsx
@@ -0,0 +1,19 @@
+import { Board } from "~/server/db/schema";
+import Button from "./button";
+
+type TaskBoardProps = {
+  board?: Board;
+};
+
+export default function TaskBoard({ board }: TaskBoardProps) {
+  if (!board?.columns?.length) {
+    return (
+      <div className="flex flex-col items-center gap-8">
+        <p className="font-heading-large text-medium-grey">
+          This board is empty. Create a new column to get started.
+        </p>
+        <Button>+ Add New Column</Button>
+      </div>
+    );
+  }
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,10 @@
 "use client";
 
-import Button from "./_components/button";
 import data from "~/server/db/data.json";
 import { useState } from "react";
 import Header from "./_components/header";
 import Sidebar from "./_components/sidebar";
+import TaskBoard from "./_components/task-board";
 
 export default function Home() {
   const [isSidebarOpen, setIsSidebarOpen] = useState(true);
@@ -20,12 +20,7 @@ export default function Home() {
             boards={data.boards}
           />
           <section className="flex flex-1 items-center justify-center">
-            <div className="flex flex-col items-center gap-8">
-              <p className="font-heading-large text-medium-grey">
-                This board is empty. Create a new column to get started.
-              </p>
-              <Button>+ Add New Column</Button>
-            </div>
+            <TaskBoard />
           </section>
         </div>
       </div>
