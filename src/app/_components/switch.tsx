51ae3ec9edcd811ef42842cc3cbf1aa6e3e9312f
Erik S. Carlsten <ecarlste@gmail.com>
2025-03-11 09:58:02 -0600

    added a transition to dark mode switch

--- src/app/_components/switch.tsx
+++ src/app/_components/switch.tsx
@@ -5,16 +5,16 @@ import { useState } from "react";
 export default function Switch() {
   const [state, setState] = useState(false);
 
-  const justify = state ? "justify-end" : "justify-start";
-
   return (
     <div
-      className={`bg-main-purple flex h-5 w-10 rounded-full p-[3px] hover:cursor-pointer ${justify}`}
+      className={`bg-main-purple flex h-5 w-10 rounded-full p-[3px] hover:cursor-pointer`}
       onClick={() => {
         setState(!state);
       }}
     >
-      <div className="h-[14px] w-[14px] rounded-full bg-white" />
+      <div
+        className={`h-[14px] w-[14px] rounded-full bg-white transition-transform duration-500 ease-in-out ${state ? "translate-x-[20px]" : "translate-x-0"}`}
+      />
     </div>
   );
 }
