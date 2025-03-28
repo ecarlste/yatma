31d48391bdd15801b554df1dbd5c5648e3eafe19
Erik S. Carlsten <ecarlste@gmail.com>
2025-03-28 15:39:15 -0600

    converted logo from image to component svg

--- frontend/src/app/_components/header.tsx
+++ frontend/src/app/_components/header.tsx
@@ -1,11 +1,9 @@
 "use client";
 
-import Image, { type StaticImageData } from "next/image";
-
-import logoDark from "~/../public/images/logo-dark.svg";
 import ActiveBoardControls from "./active-board-controls";
 import { type Board } from "~/lib/types";
 import { useTaskManagerStore } from "../_providers/task-manager-store-provider";
+import Logo from "./logo";
 
 type HeaderProps = {
   board?: Board;
@@ -20,13 +18,8 @@ export default function Header({ board, isBoardEmpty = true }: HeaderProps) {
       <div
         className={`border-r-lines-light flex h-full items-center border-r-1 transition-[min-width,padding-left] duration-500 ease-in-out ${isSidebarOpen ? "min-w-75 pl-8" : "min-w-[210px] border-b-1 pl-6"}`}
       >
-        <Image
-          src={logoDark as StaticImageData}
-          alt="Logo"
-          className="mb-2 h-6 w-fit"
-        />
+        <Logo />
       </div>
-
       <div
         className={`border-b-lines-light flex h-full w-full items-center ${board ? "justify-between" : "justify-end"} border-b-1`}
       >
