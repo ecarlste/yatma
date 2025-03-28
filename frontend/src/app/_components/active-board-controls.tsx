f2280398c6eea10adb7ed379026ce3b6e6160ba3
Erik S. Carlsten <ecarlste@gmail.com>
2025-03-28 14:45:31 -0600

    removed use of vertical ellipsis image in favor of component

--- frontend/src/app/_components/active-board-controls.tsx
+++ frontend/src/app/_components/active-board-controls.tsx
@@ -1,6 +1,5 @@
-import Image, { type StaticImageData } from "next/image";
 import Button from "./button";
-import iconVerticalEllipsis from "~/../public/images/icon-vertical-ellipsis.svg";
+import IconVerticalEllipsis from "./icon-vertical-ellipsis";
 
 type ActiveBoardControlsProps = {
   isButtonDisabled: boolean;
@@ -10,11 +9,7 @@ function ActiveBoardControls({ isButtonDisabled }: ActiveBoardControlsProps) {
   return (
     <div className="flex items-center gap-6 pr-8 pb-2">
       <Button disabled={isButtonDisabled}>+ Add New Task</Button>
-      <Image
-        src={iconVerticalEllipsis as StaticImageData}
-        alt="Vertical Ellipsis"
-        className="h-5"
-      />
+      <IconVerticalEllipsis className="shrink-0" />
     </div>
   );
 }
